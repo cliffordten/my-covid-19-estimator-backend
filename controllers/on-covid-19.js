6714ecec9e354b45695069f348f7257a01d65ceb
cliffordten <tenengclifford5@gmail.com>
2020-04-10 08:14:30 +0100

    modified controler

--- controllers/on-covid-19.js
+++ controllers/on-covid-19.js
@@ -1,64 +1,79 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const filePath = 'middleware/on-covid-19.log.txt';
+const filePath = "middleware/on-covid-19.log.txt";
 
-const EasyXml = require('easyxml');
+const EasyXml = require("easyxml");
 
 const serializer = new EasyXml({
   singularize: true,
-  rootElement: 'response',
-  dateFormat: 'ISO',
-  manifest: true
+  rootElement: "response",
+  dateFormat: "ISO",
+  manifest: true,
 });
 
-const estimator = require('../estimator');
+const dataInput = {
+  region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71,
+  },
+  periodType: "days",
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614,
+};
+
+// const dataInput = JSON.parse(dataInputs);
+
+const estimator = require("../estimator");
 
 exports.getEstimationData = (req, res) => {
-  const data = req.body;
-  const estimatorData = estimator(data);
+  const { data } = req.body;
+  let estimatorData;
 
-  if (!data) {
-    res
-      .status(200)
-      .json({ success: false, msg: 'Input data required for this request' });
+
+  if (data) {
+    estimatorData = estimator(data);
+
+  } else {
+    estimatorData = estimator(dataInput);
   }
   res.status(200).json({ success: true, estimatorData });
 };
 
 exports.getEstimationDataInJson = (req, res) => {
-  const data = req.body;
-  const estimatorData = estimator(data);
+  const { data } = req.body;
+  let estimatorData;
 
   if (!data) {
-    res
-      .status(200)
-      .json({ success: false, msg: 'Input data required for this request' });
+    estimatorData = estimator(dataInput);
+  } else {
+    estimatorData = estimator(data);
   }
   res.status(200).json({ success: true, estimatorData });
 };
 
 exports.getEstimationDataInXml = (req, res) => {
-  const data = req.body;
-  const estimatorData = estimator(data);
+  res.set("Content-Type", "text/xml");
 
-  res.set('Content-Type', 'text/xml');
+  const { data } = req.body;
+  let estimatorData;
 
   if (!data) {
-    res.status(200).send(
-      serializer.render({
-        success: false,
-        msg: 'Input data required for this request'
-      })
-    );
+    estimatorData = estimator(dataInput);
+  } else {
+    estimatorData = estimator(data);
   }
 
   res.status(200).send(serializer.render({ success: true, estimatorData }));
 };
 
 exports.getLogData = (req, res) => {
-  res.set('Content-Type', 'text/text');
+  res.set("Content-Type", "text/text");
 
-  const logData = fs.readFileSync(filePath, 'utf8');
+  const logData = fs.readFileSync(filePath, "utf8");
 
   res.status(200).send({ success: true, logData });
 };
