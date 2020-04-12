1b5fa0aef495f84883d4a1e4f80146e253732a71
cliffordten <tenengclifford5@gmail.com>
2020-04-12 19:48:05 +0100

    log formate

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,12 +4,17 @@ const fs = require('fs');
 const start = new Date();
 
 const logger = (req, res, next) => {
-  const str = (req.url).toString();
-  const seconds = (new Date() - start) / 100;
+  // const str = (req.url).toString();
+  // const seconds = (new Date() - start) / 100;
 
-  const data = `${Date.now()}\t\t${str.substr(8)}\t\tdone in ${
-    seconds.toFixed(2)
-  } seconds`;
+  // const data = `${Date.now()}\t\t${str.substr(8)}\t\tdone in ${
+  //   seconds.toFixed(2)
+  // } seconds`;
+
+  const str = Math.trunc(new Date() - start)
+
+  const data = `${req.method}\t\t${req.url}\t\t${res.statusCode}\t\t${str}ms`;
+
 
   fs.appendFile('middleware/on-covid-19.log.txt', `${data}\n`, () => {});
   next();
